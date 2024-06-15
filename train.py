f5525f45ad2b773e8f53d272a96f4ef7bf89ab78
Devansh Amin <aminpatel97@gmail.com>
2024-06-15 16:37:35 -0400

    Update train config

--- train.py
+++ train.py
@@ -1,20 +1,20 @@
-from typing import Literal, Optional
+from typing import Optional
 
 from jsonargparse import ArgumentParser, ActionConfigFile
 
+from tape.config import DatasetName, FeatureType
 from tape.data.lm_encoder import LmEncoderArgs
 from tape.data.dataset import GraphDataset
-from tape.model import NodeClassifierArgs
+from tape.gnn_model import NodeClassifierArgs
 from tape.trainer.gnn_trainer import GnnTrainerArgs, GnnTrainer
 from tape.data.llm.engine import LlmOnlineEngineArgs, LlmOfflineEngineArgs
 
 
 def get_parser() -> ArgumentParser:
-
     parser = ArgumentParser(parser_mode='omegaconf') # `omegaconf` for variable interpolation
     parser.add_argument('--config', action=ActionConfigFile)
-    parser.add_argument('--dataset', type=Literal['pubmed', 'ogb_arxiv'])
-    parser.add_argument('--feature_type', type=Literal['title_abstract', 'prediction', 'explanation', 'tape'])
+    parser.add_argument('--dataset', type=DatasetName)
+    parser.add_argument('--feature_type', type=FeatureType)
     parser.add_argument('--cache_dir', type=str)
     parser.add_argument('--seed', type=int, default=42)
     parser.add_argument('--device', type=str, required=False)
@@ -23,15 +23,10 @@ def get_parser() -> ArgumentParser:
     parser.add_argument('--llm_offline_engine', type=Optional[LlmOfflineEngineArgs], default=None)
     parser.add_argument('--gnn_model', type=NodeClassifierArgs)
     parser.add_argument('--gnn_trainer', type=GnnTrainerArgs)
-
     return parser
 
-if __name__ == '__main__':
 
-    parser = get_parser()
-    args = parser.parse_args()
-    args = parser.instantiate_classes(args)
-    print(args)
+def train(args):
 
     graph_dataset = GraphDataset(
         dataset_name=args.dataset,
@@ -43,10 +38,24 @@ if __name__ == '__main__':
         cache_dir=args.cache_dir,
         seed=args.seed,
     )
-    print(graph_dataset.dataset)
-    # trainer = GnnTrainer(
-    #     trainer_args=args.gnn_trainer,
-    #     graph_dataset=graph_dataset,
-    #     model_args=args.gnn_model,
-    # )
-    # trainer.train()
+    trainer = GnnTrainer(
+        trainer_args=args.gnn_trainer,
+        graph_dataset=graph_dataset,
+        model_args=args.gnn_model,
+    )
+    trainer.train()
+
+
+if __name__ == '__main__':
+
+    parser = get_parser()
+    args = parser.parse_args()
+    args = parser.instantiate_classes(args)
+    print(args)
+
+    if args.feature_type == FeatureType.TAPE:
+        for value in ('TA', 'P', 'E'):
+            args.feature_type = FeatureType._value2member_map_[value]
+            train(args)
+    else:
+        train(args)
